efa70d12a2b37ff472a30a851a4f643c01809200
lyubov <lyubov.krutko@gmail.com>
2018-03-21 01:48:01 -0700

    todo example delete

--- src/layout/App.js
+++ src/layout/App.js
@@ -5,7 +5,7 @@ class App extends Component {
         super(props);
 
         this.state = {
-            todoList: [],
+            todoList: ['Temp', 'New'],
             todoInput:'',
             todoInputValid: false,
         };
@@ -28,18 +28,32 @@ class App extends Component {
         this.setState({
             todoList,
         });
+        }
+        DeleteItem(todo){
+        console.log(todo);
+        const todoList = this.state.todoList.filter(el => el !== todo);
 
-    }
+        this.setState({ todoList }, () => { console.log(this.state) });
+
+        }
 
 
-  render() {
-    return (
-      <div className={'container pt-4'}>
-          <div className="row">
+
+        render() {
+         return (
+           <div className={'container pt-4'}>
+           <div className="row">
 
               <div className="col-6 col-sm-4 col-md-6 col-lg-4 border">
                   <ul>
-                      {this.state.todoList.map(el => <li key={el}>{el}</li>)}
+                      {this.state.todoList.map(el =>
+                          <li key={el}>{el}
+                            <span
+                                className="btn btn-danger btn-sm"
+                                onClick={() => this.DeleteItem(el)}
+                            >Delete
+                            </span>
+                          </li>)}
                   </ul>
 
               </div>
@@ -56,7 +70,7 @@ class App extends Component {
               <div className="input-group-append">
                   <button
                       disabled={!this.state.todoInputValid}
-                      className='btn btn-outline-primary'
+                      className='btn btn-outline-primary btn'
                       onClick={() => this.addTodo()}>Add</button>
               </div>
               </div>
